e10f66b6c4034d6d016b5fb2523e07fb20601f48
Malachi Luck <malachiluck@gmail.com>
2020-09-03 14:28:17 -0500

    done assesment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/welcome/welcome';
+import Clock from './components/clock/clock';
+import Contact from './components/contact/contact';
+import Navigation from './components/navigation/navigations'; 
+import Error from './components/error/error' ;
+import {Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+      <Route exact path="/" render={(props) => <Welcome {...props} name="Malachi" />} />
+      <Route path="/clock" component={Clock} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/error" component={Error} />
+      <Route path="/Welcome/:name" render={(props) => <Welcome {...props} name={props.match.params.name}/>} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/error/error.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Error extends React.Component {
+    render () {
+        return <div className="Error">404 Page not found</div>
+    }
+}
+
+export default Error
